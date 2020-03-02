e310cc1aae17efac00848e40b844b6cca72f6d4b
jfulle11 <jfulle11@my.westga.edu>
2020-03-02 10:44:10 -0500

    corrected qselector

--- src/project/Main.java
+++ src/project/Main.java
@@ -6,15 +6,17 @@ import qlearning.QValue;
 
 public class Main {
 	
-	public static final int TRANING_ROUNDS = 10;
+	public static final int TRANING_ROUNDS = 100000;
 
 	public static void main(String[] args) {
 		
 		MarsRover rover = new MarsRover("MarsTerrainMap1.csv");
 		ArrayList<ArrayList<QValue>> paths = rover.train(TRANING_ROUNDS);
 		
-		for(ArrayList<QValue> path : paths) {
-			System.out.println(path);
+		int start = paths.size()-5 < 0 ? 0 : paths.size()-5;
+		
+		for(int i = start; i < paths.size(); i++) {
+			System.out.println(paths.get(i));
 		}
 		
 	}
